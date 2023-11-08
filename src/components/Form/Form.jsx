a937c9648098d9b713ef443e769a21d085ea1d13
AndreyTlustenko <andreyt2018@ukr.net>
2023-11-08 20:33:10 +0100

    up2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import css from '../Styles.module.css';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
-// import { Formik, Field, Form } from 'formik';
+
 
 class Form extends Component {
   state = {
@@ -33,45 +33,7 @@ class Form extends Component {
 
   render() {
     return (
-      
-      // <Form onSubmit={this.handleSubmit} className={css.form} action="">
-      // <label className={css.label} htmlFor={this.nameInputId}>
-      //    Name{' '}
-      //   <input
-      //       className={css.input}
-      //       id={this.nameInputId}
-      //       type="text"
-      //       name="name"
-      //       value={this.state.name}
-      //       onChange={this.handleChange}
-      //       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      //       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      //       required
-      //     />{' '}
-      //   </label>
-           
-      //   <label className={css.label} htmlFor={this.numberInputId}>
-      //      Phone
-      //      <input
-      //       className={css.input}
-      //       id={this.numberInputId}
-      //       type="tel"
-      //       name="number"
-      //       value={this.state.number}
-      //       onChange={this.handleChange}
-      //       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-      //       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-      //       required
-      //     />{' '}
-      //   </label>
-      //   <Field id="lastName" name="lastName" placeholder="Doe" />
-
-      // <button className={css.button} type="submit">
-      // //     Add contact
-      // //   </button>
-      // </Form>
-   
-      <form onSubmit={this.handleSubmit} className={css.form} action="">
+                <form onSubmit={this.handleSubmit} className={css.form} action="">
         <label className={css.label} htmlFor={this.nameInputId}>
           Name{' '}
           <input
